9121b3c72ed137113a09374173cdcce3f6dd6811
Paulo Padilla Domingues <114918838+Tikualdo@users.noreply.github.com>
2026-02-08 19:49:43 +0000

    refactor: corrigiendo ejemplo de inferencia.
    Moved type inference examples into a function for better organization.

--- src/Basics/inference.ts
+++ src/Basics/inference.ts
@@ -11,14 +11,19 @@
  * determine the type of a variable based on its initial value.
  */
 
-// Explicit Declaration
-let myActualName: string = 'Cristobal';
-
-// Type Inference
-let myName = 'Cristobal'; // TS knows it is a string
-let myAge = 22;           // TS knows it is a number
-let isStudent = true;     // TS knows it is a boolean
-
-let mysteriousVariable; // ERROR in strict mode: it is an implicit 'any'
-
-let clearVariable = "Hello"; // OK: TS infers it is a string, not 'any'
+/**
+ * Infernce's example.
+ */
+function inferenceExample() {
+  // Explicit Declaration
+  let myActualName: string = 'Cristobal';
+  
+  // Type Inference
+  let myName = 'Cristobal'; // TS knows it is a string
+  let myAge = 22;           // TS knows it is a number
+  let isStudent = true;     // TS knows it is a boolean
+  
+  let mysteriousVariable; // ERROR in strict mode: it is an implicit 'any'
+  
+  let clearVariable = "Hello"; // OK: TS infers it is a string, not 'any'
+}
